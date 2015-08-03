907832997cd606f36fd5e588e2ed10d2927d2a0a
Pranay Kotapi <pranayshinoda@gmail.com>
2015-08-04 00:11:56 +0530

    Added supp files

---
+++ app/build/generated/source/buildConfig/debug/app/com/rockvile/ironman_repulsorflashlight/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.com.rockvile.ironman_repulsorflashlight;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "app.com.rockvile.ironman_repulsorflashlight";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2.0";
+}
